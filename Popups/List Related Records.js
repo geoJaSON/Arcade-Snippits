85d3cc4b3c1336c80f309b05c202a8b3521ed53f
Jason Jordan <47825739+geoJaSON@users.noreply.github.com>
2023-02-17 11:32:21 -0600

    Update List Related Records.js

--- Popups/List Related Records.js
+++ Popups/List Related Records.js
@@ -3,10 +3,8 @@ var popupString = ''
 
 for (var f in FeatureSetByRelationshipName($feature,"DR_4619_CALDOR_FIRE_Debris_visits")){
         popupString += "Date: " + Text(f.visit_date, 'DD MMM YY') + TextFormatting.NewLine + 
-        "Safety Concern: " +
-        DefaultValue(f.safety_concerns, 'no data') + TextFormatting.NewLine +
-        "Environmental Concern: " +
-        DefaultValue(f.environmental_concerns, 'no data') + TextFormatting.NewLine + TextFormatting.NewLine
+        "Safety Concern: " + DefaultValue(f.safety_concerns, 'no data') + TextFormatting.NewLine +
+        "Environmental Concern: " + DefaultValue(f.environmental_concerns, 'no data') + TextFormatting.NewLine + TextFormatting.NewLine
 }
 
 return popupString
