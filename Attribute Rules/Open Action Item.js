0ee3a73ae538094ccb5d28a13ee5a8394662f8de
Jason Jordan <47825739+geoJaSON@users.noreply.github.com>
2024-05-09 12:19:16 -0500

    Update Open Action Item.js

--- Attribute Rules/Open Action Item.js
+++ Attribute Rules/Open Action Item.js
@@ -1,15 +1,19 @@
 if ($feature.subject=='Complaint'){
-    return {
-      "edit": [{
-          "className": "complaints",
-          "adds": [
-            {"globalID": Guid(), "attributes": {"situation": $feature.comments,"parentglobalid": $feature.parentglobalid}}
-          ]}]}}
-    
-    else if ($feature.followuprequired =='Yes'){
-    return {
-      "edit": [{
-          "className": "actionitems",
-          "adds": [
-            {"globalID": Guid(), "attributes": {"actionneeded":"Callback","comments": $feature.comments,"itemassignedteam": 'Call Center',"parentglobalid": $feature.parentglobalid}}
-          ]}]}}
+var querytext = "GlobalID = '" + Text($feature.parentglobalid)+"'"
+var fs = First(Filter(FeatureSetByName($datastore, 'BLUE_ROOF.BLUE_ROOF_OWNER.roe', ["appfirstname","applastname","appprimaryphone","datecontractorcomplete"], false),querytext))
+if (fs['datecontractorcomplete'] != null){var roof = 'Roof Not Installed'}
+else {var roof = 'Roof Installed'}
+return {
+  "edit": [{
+      "className": "BLUE_ROOF.BLUE_ROOF_OWNER.complaints",
+      "adds": [
+        {"globalID": Guid(), "attributes": {"situation": $feature.comments,"complaintroeidpk":$feature.callroeidpk,"parentglobalid": $feature.parentglobalid,"primarycontactphone":fs['appprimaryphone'],"primarycontactname":fs['appfirstname']+" "+fs['applastname'],"roofstatus":roof}}
+      ]}]}}
+
+else if ($feature.followuprequired =='Yes'){
+return {
+  "edit": [{
+      "className": "BLUE_ROOF.BLUE_ROOF_OWNER.actionitems",
+      "adds": [
+        {"globalID": Guid(), "attributes": {"actionneeded":"Callback","comments": $feature.comments,"itemassignedteam": 'Call Center',"parentglobalid": $feature.parentglobalid,"actionroeidpk":$feature.callroeidpk,"itemdateopened": $feature.created_date}}
+      ]}]}}
